16bc8a6f8aab718c01b7cca29aa0eae85d473cfd
CosmicCoder96 <abhinav.khare31@gmail.com>
2018-05-13 14:48:50 +0530

    Doc Strings

--- media.py
+++ media.py
@@ -1,10 +1,16 @@
 class Movie:
 	def __init__(self, title, poster_image_url, trailer_youtube_url):
+		"""
+			:param title: Title of the Movie
+			:param poster_image_url: URL for the poster image of the movie
+			:param trailer_youtube_url: URL for the youtube trailer of the movie
+		"""
 		self.title = title
 		self.poster_image_url = poster_image_url
 		self.trailer_youtube_url = trailer_youtube_url
 	
 	def __repr__(self):
+		# A representation of a Movie Object is specified here
 		return "<Title: {}, Poster Url: {}, youtube trailer url: {}>".format(self.title, self.poster_image_url, self.trailer_youtube_url)
 	
 	
