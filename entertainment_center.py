41c4d74e4eac96941772a35bb592437ff0a02a02
CosmicCoder96 <abhinav.khare31@gmail.com>
2018-05-13 10:11:39 +0530

    Add flair

--- entertainment_center.py
+++ entertainment_center.py
@@ -2,12 +2,10 @@ import media
 import requests
 import json
 import fresh_tomatoes
-movie1 = media.Movie('THOR', 'https://lalala', "v(adk")
 
 movieList = []
 configUrl = "https://api.themoviedb.org/3/configuration?api_key=d52b401aad5afca8dc068a00604370d4"
 movieUrl = "https://api.themoviedb.org/3/discover/movie?api_key=d52b401aad5afca8dc068a00604370d4&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=true&page=1"
-trailerUrl = ""
 
 movieResponse = requests.get(movieUrl)
 configResponse = requests.get(configUrl)
@@ -23,10 +21,10 @@ if configResponse.ok:
                 trailerJsonData = json.loads(trailerResponse.content)
                 try:
                     youtubeUrl = "https://www.youtube.com/watch?v=" + trailerJsonData['results'][0]['key']
-                    movieList.append(media.Movie(result['title'], basePosterUrl + basePosterSize + result['poster_path'], youtubeUrl))
-                    print("Please wait .. fetching movies from the API\n")
+                    print("Please wait .. fetching movies from the API\n")                    
+                    movie = media.Movie(result['title'], basePosterUrl + basePosterSize + result['poster_path'], youtubeUrl)
+                    print("Fetched: {}".format(movie))
+                    movieList.append(movie)
                 except:
                     continue
-    for movie in movieList:
-        print(movie)
     fresh_tomatoes.open_movies_page(movieList)
