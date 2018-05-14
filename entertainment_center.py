c5c2a98a562646ab76fb1e36f0191cbffbd5eca4
CosmicCoder96 <abhinav.khare31@gmail.com>
2018-05-14 11:30:43 +0530

    PEP8

--- entertainment_center.py
+++ entertainment_center.py
@@ -6,35 +6,37 @@ import fresh_tomatoes
 # An empty list is initialised which will later store all the movie obects
 movieList = []
 
-# configUrl stores the URL which fetches configuration from The Movie DB API. It is required to get Base Url for the poster images.
-configUrl = "https://api.themoviedb.org/3/configuration?api_key=d52b401aad5afca8dc068a00604370d4"
+configUrl = "https://api.themoviedb.org/3/configuration?api_key=d52b401aad5afca8dc068a00604370d4"  # NOQA
+movieUrl = "https://api.themoviedb.org/3/discover/movie?api_key=d52b401aad5afca8dc068a00604370d4&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=true&page=1"  # NOQA
 
-# movieUrl stores the URL which fetches a single page of movies from The Movie DB API based on their popularity in descending order.
-movieUrl = "https://api.themoviedb.org/3/discover/movie?api_key=d52b401aad5afca8dc068a00604370d4&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=true&page=1"
-
-# make a GET request to API to fetch the movies
+# make GET requests to API to fetch movies and basic poster config
 movieResponse = requests.get(movieUrl)
-
-# make a GET request to API to fetch the configuration
 configResponse = requests.get(configUrl)
 
 
-if configResponse.ok:  # Check if a success status of 200 is returned.
-    jsonData = json.loads(configResponse.content) # parses the json response of the API
-    basePosterUrl = jsonData['images']['secure_base_url'] # fetch the secure base url of the posters which is required for fetching the image
-    basePosterSize = 'w500' # specify the size of the image desired
-    if movieResponse.ok: # Check if a success status of 200 is returned while fetching movies.
-        movieJsonData = json.loads(movieResponse.content) # Parse the json data of the movies fetched from the API
-        for result in movieJsonData['results']: # Iterate over all the movies fetched from the API
-            trailerResponse = requests.get("https://api.themoviedb.org/3/movie/"+str(result['id'])+"/videos?api_key=d52b401aad5afca8dc068a00604370d4&language=en-US") # Make a request for the movie's trailer
-            if trailerResponse.ok: # Check if the trailer of the movie is successfully retrieved
-                trailerJsonData = json.loads(trailerResponse.content) # Parse the json trailer response 
+if configResponse.ok:
+    # parse config JSON data and specify poster size
+    jsonData = json.loads(configResponse.content)
+    basePosterUrl = jsonData['images']['secure_base_url']
+    posterSize = 'w500'
+    if movieResponse.ok:
+        # parse movie JSON data and iteratively fetch movie trailer
+        movieJsonData = json.loads(movieResponse.content)
+        for result in movieJsonData['results']:
+            trailerResponse = requests.get("https://api.themoviedb.org/3/movie/"+str(result['id'])+"/videos?api_key=d52b401aad5afca8dc068a00604370d4&language=en-US")  # NOQA
+            if trailerResponse.ok:
+                trailerJsonData = json.loads(trailerResponse.content)
                 try:
-                    youtubeUrl = "https://www.youtube.com/watch?v=" + trailerJsonData['results'][0]['key'] # Generate the youtube URL of the movie trailer
-                    print("Please wait .. fetching movies from the API\n") # Helper text               
-                    movie = media.Movie(result['title'], basePosterUrl + basePosterSize + result['poster_path'], youtubeUrl) # Create a movie object
-                    print("\033[92m Fetched: \033[94m{}\033[0m".format(movie)) # Helper text
-                    movieList.append(movie) # Append the obect to the movie list
+                    # Generate youtube URL
+                    videoId = trailerJsonData['results'][0]['key']
+                    youtubeUrl = "https://www.youtube.com/watch?v=" + videoId
+                    print("Please wait .. fetching movies from the API\n")
+                    posterUrl = posterUrl + posterSize + result['poster_path']
+                    # Create Movie objects and append to movie list
+                    movie = media.Movie(result['title'], posterUrl, youtubeUrl)
+                    print("\033[92m Fetched: \033[94m{}\033[0m".format(movie))
+                    movieList.append(movie)
                 except:
                     continue
-    fresh_tomatoes.open_movies_page(movieList) # Call the ooen_movies_page function with the list of movies as it's parameter
+    # Call to open_movies_page function with movie list
+    fresh_tomatoes.open_movies_page(movieList)
